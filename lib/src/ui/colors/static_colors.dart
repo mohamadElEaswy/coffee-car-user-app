c91d1d88a6daf6ea12869311772f5589f1b457f3
Muhamad El Easwy <midoo_303@yahoo.com>
2022-01-30 20:32:26 +0200

    fixing adding firebase ERRORS

--- lib/src/ui/colors/static_colors.dart
+++ lib/src/ui/colors/static_colors.dart
@@ -4,6 +4,8 @@ class GlobalStaticColors{
   static MaterialColor primaryColor =  Colors.indigo;
   static const Color logoColor = Color(0xff7DD899);
   static const Color appBarColor = Color(0xffffffff);
+  static const Color debBlue = Color(0xfff022741);
+
 }
 
 //#1C2124
---
+++ lib/src/ui/pages/providers/providers_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProvidersPage extends StatelessWidget {
+  const ProvidersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
